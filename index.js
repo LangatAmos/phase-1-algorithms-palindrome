5a8858446e02a5c3aa8d1a6e65642feabc177eb2
Amos Langat <amos.langat@student.moringaschool.com>
2022-08-26 15:53:12 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const stringArray = word.split('');
+  const reversedstringArray = stringArray.reverse();
+  const reversedWord = reversedstringArray.join('');
+
+  if (reversedWord === word) {
+    return true;
+
+}else{
+  return false;
+}
 }
 
 /* 
   Add your pseudocode here
+  create a new function that takes a string and returns  reversed string
+  write a function to reverse the string
+  write conditional statement
+
 */
 
 /*
@@ -21,5 +35,4 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
